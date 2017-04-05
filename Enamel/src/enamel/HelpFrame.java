21c4ca9f5a046ba32d3ee3765d6a1186fb8a6ee5
Siddharth Bhadrwaj <sid16@my.yorku.ca>
2017-04-05 00:46:42 -0500

    Adding the class that contains Help Content Frame

---
+++ Enamel/src/enamel/HelpFrame.java
@@ -0,0 +1,44 @@
+package enamel;
+
+
+import java.awt.BorderLayout;
+	 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+	 
+	public class HelpFrame extends JFrame{
+	       
+	        public JTextArea helpTextArea;
+	     	       
+	        public HelpFrame(){
+	                setTitle("Help-Team 3's Authoring App");
+	               
+	                helpTextArea = new JTextArea("This section provides information about each feature that deals with editing the Senario file.\n\n"
+	                		+ "1) Add 'Normal text': Add text using this option.\n\n"
+	                		+ "2) Add 'Sound file': Load a sound file(.wav) by selecting this option.\n\n"
+	                		+ "3) Add 'Display char': Display a specific optionchar adding this option\n\n"
+	                		+ "4) Add 'Skip-button': Skip a button by adding this option\n\n"
+	                		+ "5) Add 'Skip to': Skip to a specific section by selecting this option\n\n"
+	                		+ "6) Add 'Get user-input': Get the user input by selecting this option\n\n"
+	                		+ "7) Add 'Repeat': Repeat a specifc function by selecting this option\n\n"
+	                		+ "8) Add 'Reset buttons': Reset all the buttons by selecting this option\n\n"
+	                		+ "9) Add 'Clear all display': Clear everything currently being displayed back to its initial setting by selecting this option\n\n"
+	                		+ "10) Button 'Edit': Turn on the editing capabilites by selecting this option\n\n"
+	                		+ "11) Button 'Delete': Delete the current line in the senario file being displayed, by selecting this option\n\n"
+	                		+ "12) Button 'Up': Move the text selector to the above line in the selected senario by selecting this option\n\n"
+	                		+ "13) Button 'Down': Move the text selector to the line below in the selected senario by selecting this option\n\n"
+	                		+ "More Options:\ta) Add 'Set Voice': Set the voice by selecting this option\n\t"
+	                		+ "b) Add 'Repeat Button': Repeat a button by adding this option\n\t"
+	                		+ "c) Add 'Display cell pins': Display cell pins by adding this option\n\t"
+	                		+ "d) Add 'Raise one pin': Raise a specific pin by selecting this option\n\t"
+	                		+ "e) Add 'Lower one pin': Lower a specific pin by selecting this option\n\t"
+	                		+ "f) Add 'Clear one cell': Clear on cell, lowering all pins in it, by adding this option");
+	                helpTextArea.setEditable(false);
+	                helpTextArea.setLineWrap(true);
+	                helpTextArea.setWrapStyleWord(true);
+	                getContentPane().add(helpTextArea, BorderLayout.CENTER);
+	               
+	        }
+	 
+	}
+
