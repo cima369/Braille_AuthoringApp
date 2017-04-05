e5590697b0bab7f647c613b4250b59ba5c8f8f03
Siddharth Bhadrwaj <sid16@my.yorku.ca>
2017-04-05 10:07:38 -0500

    minor changes to format of text

--- Enamel/src/enamel/HelpFrame.java
+++ Enamel/src/enamel/HelpFrame.java
@@ -25,11 +25,11 @@ import javax.swing.JTextArea;
 	                		+ "9) Button 'Delete': Delete the current line in the senario file being displayed, by selecting this option\n\n"
 	                		+ "10) Button 'Up': Move the text selector to the above line in the selected senario by selecting this option\n\n"
 	                		+ "11) Button 'Down': Move the text selector to the line below in the selected senario by selecting this option\n\n"
-	                		+ "More Options:\ta) Add 'Set Voice': Set the voice by selecting this option\n\t"
-	                		+ "b) Add 'Repeat Button': Repeat a button by adding this option\n\t"
-	                		+ "c) Add 'Display cell pins': Display cell pins by adding this option\n\t"
-	                		+ "d) Add 'Raise one pin': Raise a specific pin by selecting this option\n\t"
-	                		+ "e) Add 'Lower one pin': Lower a specific pin by selecting this option\n\t"
+	                		+ "More Options:\ta) Add 'Set Voice': Set the voice by selecting this option\n\n\t"
+	                		+ "b) Add 'Repeat Button': Repeat a button by adding this option\n\n\t"
+	                		+ "c) Add 'Display cell pins': Display cell pins by adding this option\n\n\t"
+	                		+ "d) Add 'Raise one pin': Raise a specific pin by selecting this option\n\n\t"
+	                		+ "e) Add 'Lower one pin': Lower a specific pin by selecting this option\n\n\t"
 	                		+ "f) Add 'Clear one cell': Clear on cell, lowering all pins in it, by adding this option");
 	                helpTextArea.setEditable(false);
 	                helpTextArea.setLineWrap(true);
