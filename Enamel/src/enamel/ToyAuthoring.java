72f02069cc80d7c030cf8a50d0461ec99da44990
Eric Dao <ericdao1610@gmail.com>
2017-04-05 22:22:40 -0400

    Changed toy authoring to play the current scenario file of the program
    Also changed scenario one by removing the set voices.

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -5,7 +5,7 @@ public class ToyAuthoring {
 	public static void main(String[] args) {
 		SoundPlayer s = new SoundPlayer();
         //s.setScenarioFile("/Users/bil/York/workspace/git/LaSSoftE/Enamel/SampleScenarios/Scenario_One.txt");
-        s.setScenarioFile("SampleScenarios/ok.txt");
+        s.setScenarioFile("SampleScenarios/temp.txt");
 	}
 
 }
